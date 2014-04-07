a3079f5996b6bfb858387a16eefafa13b72b5b4c
barneycolby <barnaby_colby@hotmail.com>
2014-04-07 15:45:22 +0100

    Moved printUsageInstructions so that main is at top of file

--- src/recall.cpp
+++ src/recall.cpp
@@ -2,16 +2,19 @@
 
 using namespace std;
 
-/**
- * Prints the usage instructions of the program
- */
-void printUsageInstructions() {
-	cout << "Usage: recall fileContainingThoughts" << endl;
-}
+// Forward declaration of printUsageInstructions
+void printUsageInstructions();
 
 int main(int argc, char* argv[]) {
 	// Check if we were given the number of arguments we expected
 	if (argc != 2) {
 		printUsageInstructions();
 	}
+}
+
+/**
+ * Prints the usage instructions of the program
+ */
+void printUsageInstructions() {
+	cout << "Usage: recall fileContainingThoughts" << endl;
 }
