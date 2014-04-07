5e02dd56be1ed907c1b6d95370460589edd44f7e
barneycolby <barnaby_colby@hotmail.com>
2014-04-07 17:43:21 +0100

    Added basic parsing to check whether the file contains valid xml

--- src/NotesTree.cpp
+++ src/NotesTree.cpp
@@ -1,9 +1,20 @@
 #include "NotesTree.h"
 
+#include <iostream>
+#include "pugixml.hpp"
+
+using namespace std;
+
 /**
  * \brief Constructs a NotesTree object from a file
  * \detail Constructs a NotesTree object from a file, by parsing it and creating a tree that reflects the data in the file
+ * \param filePath The path of the file to construct the NotesTree object from
  */
-NotesTree::NotesTree(const char*) {
-	// Do nothing
+NotesTree::NotesTree(const char* filePath) {
+	// Parse the xml file
+	pugi::xml_document doc;
+	if(!doc.load_file(filePath)) {
+		// The document could not be parsed
+		cout << "Failed to parse " << filePath << endl;
+	}
 }
