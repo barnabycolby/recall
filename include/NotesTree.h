1b864d9976f80869f779b8fb024f2418b8c5f12a
Barnaby Colby <barneycolby@gmail.com>
2014-04-08 21:42:36 +0100

    Added header guard

--- include/NotesTree.h
+++ include/NotesTree.h
@@ -1,3 +1,6 @@
+#ifndef NOTES_TREE_H
+#define NOTES_TREE_H
+
 /**
  * \author Barnaby Colby
  * \brief Stores notes in a tree
@@ -7,3 +10,5 @@ class NotesTree {
 public:
 	NotesTree(const char* filePath);
 };
+
+#endif
